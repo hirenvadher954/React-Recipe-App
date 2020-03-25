5f1177986f8a5fa3ea4de1b56e1aba6398ed39f3
Hiren Vadher <hirenvadher954@gmail.com>
2020-03-25 13:12:21 +0530

    CSS Added

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './recipe.module.css';
 
 function Recipe({title, calories, image, ingredients}) {
     return (
-        <div>
+        <div className={style.recipe}>
             <h1>{title}</h1>
             <ol>
                 {
@@ -10,8 +11,8 @@ function Recipe({title, calories, image, ingredients}) {
                         ingredient => <li>{ingredient.text}</li>)
                 }
             </ol>
-            <p>{calories}</p>
-            <img alt={title} src={image}/>
+            <p>Calories : {calories}</p>
+            <img className={style.image} alt={title} src={image}/>
         </div>
     );
 }
